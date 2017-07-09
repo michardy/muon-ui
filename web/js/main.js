e8fc1df3573d8be767f80a255f731efe79fe21b6
Michael Hardy <98.michael.hardy@gmail.com>
2017-07-09 16:47:28 -0700

    Added ffi calls

--- web/js/main.js
+++ web/js/main.js
@@ -1,23 +1,10 @@
-class Queue {
-	constructor() {
-		this.mq = [];
-	}
-	add_messages(messages) {
-		for (m in messages.split('\r\n')) {
-			this.mq.push();
-		}
-	}
-	get_message() {
-		return this.mq.pop();
-	}
-}
-
-q= new Queue();
-
 var ws = new WebSocket("ws://"+window.location.host+"/socket");
 ws.onopen = function() {
 	ws.send("Hello Server");
 };
+
+var queue_push = Module.cwrap('queue_push', 'null', ['string']);
+
 ws.onmessage = function (evt) {
-	q.add_messages(evt.data);
+	queue_push(evt.data);
 };
