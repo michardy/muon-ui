83a6390444e73a0eddb81c9a5df07f95fb9fdc24
Michael Hardy <98.michael.hardy@gmail.com>
2017-07-09 19:17:23 -0700

    Classes for received data

--- dataprocessing/dataprocessing.cpp
+++ dataprocessing/dataprocessing.cpp
@@ -2,10 +2,102 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <ctime>
 
 std::vector<std::string> queue; //can't use char[72] so we use a string
 
 extern "C" void queue_push (char *test);
+
+class risingedgecount {
+	// bits 0-4: TMC count whatever that means
+	// Units? Who knows
+	// Endianness?
+	unsigned char tmc;
+	// bit 5: is it a rising edge
+	bool re;
+	// Its reserved, OR ELSE
+	bool reserved;
+	// Is it a new event
+	bool event_new;
+};
+
+class fallingedgecount {
+	// bits 0-4: TMC count whatever that means
+	// Units? Who knows
+	// Endianness?
+	unsigned char TMC;
+	// bit 5: is it a rising edge
+	bool RE;
+};
+
+class daqstatus {
+	// Endianness?
+	
+	// bit 0: is the PPS count up to date
+	bool ppsc;
+	
+	// bit 1: is the trigger (what's this?)
+	bool trig_interrrupt;
+	
+	// bit 2: is the GPS data corrupted
+	bool gps_corrupt;
+	
+	// bit 3: I have to quote here
+	// "Current or last 1PPS rate is not within 41666666 ±50 CPLD clock tick.s (This is a result of a GPS glitch, the DAQ uC being busy, or the CPLD oscillator not tuned correctly.)"
+	bool cpld_pps_mismatch;
+};
+
+class message {
+	// Do we use little endian or big?  It would be nice to know!
+	
+	// 32 bit clock trigger count. Do not read data if value is 0
+	// What does it mean? Only God knows
+	// Endianness?
+	unsigned long trigger;
+	
+	// Count of rising edge @ input 0
+	risingedgecount re0;
+	
+	// Count of falling edge @ input 0
+	fallingedgecount fe0;
+	
+	// Count of rising edge @ input 1
+	risingedgecount re1;
+	
+	// Count of falling edge @ input 1
+	fallingedgecount fe1;
+	
+	// Count of rising edge @ input 2
+	risingedgecount re2;
+	
+	// Count of falling edge @ input 2
+	fallingedgecount fe2;
+	
+	// Count of rising edge @ input 3
+	risingedgecount re3;
+	
+	// Count of falling edge @ input 4
+	fallingedgecount fe3;
+	
+	// CPLD count of time pulse from GPS
+	// Endianness?
+	unsigned long CPLD;
+	
+	// Time of the last GPS time update
+	time_t gps_update;
+	
+	// Is the GPS data valid
+	bool gps_valid;
+	
+	// Number of visible GPS satellites
+	unsigned char sat_num;
+	
+	// DAQ status
+	daqstatus daqstat;
+	
+	// GPS to PPS skew
+	short skew;
+};
 
 void queue_push (char *test) {
 	std::stringstream s;
