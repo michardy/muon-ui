66c27bef869f412eb1a46e9625550a3bafdeafa0
Michael Hardy <98.michael.hardy@gmail.com>
2017-07-16 15:20:04 -0700

    More layout

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,3 +1,8 @@
+function graph(pos, value) {
+	var ga = document.getElementById("grapharea");
+	gc = ga.getContext('2d');
+}
+
 var ws = new WebSocket("ws://"+window.location.hostname+":8888/socket");
 ws.onopen = function() {
 	ws.send("Hello Server");
