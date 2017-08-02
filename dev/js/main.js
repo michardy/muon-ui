937163cca2cdac307b01dee5541632f929cea467
Michael Hardy <98.michael.hardy@gmail.com>
2017-08-02 16:03:21 -0700

    Graph displays correct number of lines

--- dev/js/main.js
+++ dev/js/main.js
@@ -5,66 +5,57 @@ var pos = 1000;
 
 /*
  * Ze Magic Constants
- * DO NOT TOUCH
+ * DO NOT TOUCH!
+ * Based on trial and error, vodoo magic and some limited logic.  
+ * 
+ * Yes, I know it breaks on IOS devices and some Androids in portrait mode.
+ * But this was built for TVs
  */
 const GRAPH_HEIGHT = graphcont.offsetHeight;
 const LINE_WIDTH = (GRAPH_HEIGHT/99).toString();
 const GRAPH_LINES = [
-	((GRAPH_HEIGHT/4)-(GRAPH_HEIGHT/17)).toString(),
-	(((2*GRAPH_HEIGHT)/4)-(LINE_WIDTH/2)).toString(),
-	(((3*GRAPH_HEIGHT)/4)+(GRAPH_HEIGHT/35)).toString(),
+	((GRAPH_HEIGHT/5)-(GRAPH_HEIGHT/17)).toString(),
+	(((2*GRAPH_HEIGHT)/5)-(GRAPH_HEIGHT/35)).toString(),
+	(((3*GRAPH_HEIGHT)/5)+(GRAPH_HEIGHT/90)).toString(),
+	(((4*GRAPH_HEIGHT)/5)+(GRAPH_HEIGHT/27)).toString()
 ];
 const LINE_LENGTH = graphcont.offsetWidth;
 
 
-var state_0 = false;
-var state_1 = false;
-var state_2 = false;
-var state_3 = false;
 
-var index_0 = Module.cwrap('get_index_0', 'bool', []);
-var index_1 = Module.cwrap('get_index_1', 'bool', []);
-var index_2 = Module.cwrap('get_index_2', 'bool', []);
-var index_3 = Module.cwrap('get_index_3', 'bool', []);
+const LINE_COLORS = [
+	'#f00',
+	'#ff0',
+	'#0f0',
+	'#00f',
+]
+
+var state = [
+	false,
+	false,
+	false,
+	false
+];
+
+var index = [
+	Module.cwrap('get_index_0', 'number', []),
+	Module.cwrap('get_index_1', 'number', []),
+	Module.cwrap('get_index_2', 'number', []),
+	Module.cwrap('get_index_3', 'number', [])
+]
 
 console.log();
 
-function graph() {//index_0, index_1, index_2, index_3) {
-	if (index_0()) {
-		
-	} else {
-		var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'path');
-		newElement.setAttribute("d","M 0 "+GRAPH_LINES[0]+" L "+ LINE_LENGTH +" "+GRAPH_LINES[0]);
-		newElement.style.stroke = "#f00";
-		newElement.style.strokeWidth = (LINE_WIDTH+"px");
-		graphCTX.appendChild(newElement);
-	}
-	if (index_1()) {
-		
-	} else {
-		var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'path');
-		newElement.setAttribute("d","M 0 "+GRAPH_LINES[1]+" L "+ LINE_LENGTH +" "+GRAPH_LINES[1]);
-		newElement.style.stroke = "#0f0";
-		newElement.style.strokeWidth = (LINE_WIDTH+"px");
-		graphCTX.appendChild(newElement);
-	}
-	if (index_2()) {
-		
-	} else {
-		var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'path');
-		newElement.setAttribute("d","M 0 "+GRAPH_LINES[2]+" L "+ LINE_LENGTH +" "+GRAPH_LINES[2]);
-		newElement.style.stroke = "#00f";
-		newElement.style.strokeWidth = (LINE_WIDTH+"px");
-		graphCTX.appendChild(newElement);
-	}
-	if (index_3()) {
-		
-	} else {
-		/*var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'path');
-		newElement.setAttribute("d","M 0 "+GRAPH_LINES[2]+" L "+ LINE_LENGTH +" "+GRAPH_LINES[0]);
-		newElement.style.stroke = "#00f";
-		newElement.style.strokeWidth = (LINE_WIDTH+"px");
-		graphCTX.appendChild(newElement);*/
+function graph() {
+	for (var i = 0; i < 4; i++){
+		if (index[i] === 1) {
+		} else {
+			var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+			newElement.setAttribute("d","M 0 "+GRAPH_LINES[i]+" L "+ LINE_LENGTH +" "+GRAPH_LINES[i]);
+			newElement.style.stroke = LINE_COLORS[i];
+			newElement.style.strokeWidth = (LINE_WIDTH+"px");
+			graphCTX.appendChild(newElement);
+		}
 	}
 }
 
@@ -79,4 +70,4 @@ ws.onmessage = function (evt) {
 	queue_push(evt.data);
 };
 
-setInterval(graph, 10);//, index_0, index_1, index_2, index_3)
+setInterval(graph, 10);
